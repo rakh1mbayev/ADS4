cfbd988b7c8a62e6ae02b9046e2abe5f552ae255
Bekzhan <114474251+rakh1mbayev@users.noreply.github.com>
2024-05-24 20:53:47 +0500

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices; 
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) { 
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
